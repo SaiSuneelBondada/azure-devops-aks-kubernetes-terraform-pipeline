3dd7aefed2f716571541481936aa4e14f1955b3d
Naga Veera Venu Sai Suneel Bondada <69857096+SaiSuneelBondada@users.noreply.github.com>
2025-06-29 15:42:37 +0530

    Update 02-variables.tf

--- terraform-manifests/02-variables.tf
+++ terraform-manifests/02-variables.tf
@@ -14,7 +14,7 @@ variable "location" {
 variable "resource_group_name" {
   type = string
   description = "This variable defines the Resource Group"
-  default = "terraform-aks"
+  default = "terraform-storage-rg"
 }
 
 # Azure AKS Environment Name
